f88154529f0ae1f35d71cf1ada91828bcf01f112
Albert Botashev <abotashev@fun.co>
2021-10-11 22:13:48 +0300

    fixes

--- app/migrations/Version20211005211345.php
+++ app/migrations/Version20211005211345.php
@@ -27,7 +27,7 @@ final class Version20211005211345 extends AbstractMigration
         $this->addSql('CREATE TABLE insurance_objects_types (id INT AUTO_INCREMENT NOT NULL, type VARCHAR(100) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE insurance_objects_types_fields (id INT AUTO_INCREMENT NOT NULL, insurance_object_type_id INT NOT NULL, field_name VARCHAR(100) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE client ADD CONSTRAINT FK_C74404559D86650F FOREIGN KEY (user_id_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE client ADD CONSTRAINT FK_C74404559D86650F FOREIGN KEY (user_id) REFERENCES user (id)');
     }
 
     public function down(Schema $schema): void
